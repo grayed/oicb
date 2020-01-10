0697339c657b0222e8f3d6966fe20786e18278c0
Vadim Zhukov <persgray@gmail.com>
2020-01-10 11:09:01 +0300

    More checks when preparing release.

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,28 +2,42 @@
 
 set -e
 
-if [ $# -eq 0 ]; then
-	echo "usage: ${0##*/} tag [savepath]" >&2
+err() {
+	for e in "$@"; do
+		echo "$e" >&2
+	done
 	exit 1
-fi
+}
+
+usage() {
+	err "$@" "usage: ${0##*/} tag [savepath]"
+}
+
+test $# -gt 0 || usage
+test $# -le 2 || usage
 
 tag=$1; shift
-if ! echo "$tag" | egrep -q '^v[0-9]+\.[0-9]+\.[0-9]+$'; then
-	echo "invalid tag name, must be like v1.2.3" >&2
-	exit 1
-fi
+echo "$tag" | egrep -q '^v[0-9]+\.[0-9]+\.[0-9]+$' ||
+	err "invalid tag name, must be like v1.2.3"
 
-arc_path=/tmp
-if [ $# -gt 0 ]; then
-	arc_path=$1; shift
-fi
+arc_path=${1:-/tmp}
 
 v=${tag#v}
 arc_name=oicb-${v}.tar.gz
 
+test -f CHANGES || err "${0##*/} must be called from source root"
+! grep -Fqx "HEAD" CHANGES || err "CHANGES must not contain HEAD section"
+  grep -Fqx "v.$v" CHANGES || err "CHANGES lacks $v section"
+
+git branch | grep -Fqx '* master' || err "not on master branch"
+
 if git tag | fgrep -qx "$tag"; then
 	echo "tag $tag is already there, skipping it's creation"
 else
+	if git status -s CHANGES | grep 'M CHANGES'; then
+		git ci CHANGES -m "version $v"
+		git push origin
+	fi
 	git tag "$tag"
 	git push origin "$tag"
 fi
